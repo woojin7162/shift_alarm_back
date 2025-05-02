930c96759008b7203c1073718345e9f051c4fb14
woojin7162 <woojin7162@naver.com>
2025-05-03 03:28:33 +0900

    Update index.js

--- index.js
+++ index.js
@@ -192,7 +192,7 @@ app.get('/shifts', (req, res) => {
 });
 
 // 서버 시작
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
